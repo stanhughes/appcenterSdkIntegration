5fac98490c18a74b4ef4315c9c12eefbfe5b67fa
Stan Hughes <stanley.hughe.tensar@gmail.com>
2020-09-24 19:58:16 -0400

    Peronalized the test app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+    return (
+        <View style={{ flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'lightblue'}}>
+            <Text style={{ color:'dodgerblue', fontWeight:'200', fontSize:25}}>Stan's</Text>
+            <Text style={{ fontWeight:'bold', fontSize:32, color:'dodgerblue'}}>App Center Test</Text>
+        </View>
+    )
+}
+
+export { App }
