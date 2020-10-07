ae985a29a7ce6e2154c8181e7adf7d839e596999
Stan Hughes <stanley.hughe.tensar@gmail.com>
2020-10-07 09:13:42 -0400

    Change screen display
    Make sure it is updating

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
     return (
         <View style={{ flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'lightblue'}}>
             <Text style={{ color:'dodgerblue', fontWeight:'200', fontSize:25}}>Stan's</Text>
-            <Text style={{ color:'dodgerblue', fontWeight:'200', fontSize:25}}>Try 100</Text>
+            <Text style={{ color:'dodgerblue', fontWeight:'200', fontSize:25}}>Oct 7 9:12</Text>
             <Text style={{ fontWeight:'bold', fontSize:32, color:'dodgerblue'}}>App Center Test</Text>
         </View>
     )
